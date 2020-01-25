cd6312b4ee9e509dcf119ac43e6999e3a4b1bccc
alex <alexiscdmovil@live.com>
2020-01-25 07:56:52 -0500

    REPARACION DE LOGIN

---
+++ app/src/main/java/com/example/ubernavidenio/login_view_controler.java
@@ -0,0 +1,14 @@
+package com.example.ubernavidenio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_view_controler extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_view_controler);
+    }
+}
