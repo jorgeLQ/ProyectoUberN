ab9867fd4aa55e22aeaac2b768cb433c1d21e531
alex <alexiscdmovil@live.com>
2020-01-25 13:51:43 -0500

    Registro cuenta mejorado

--- app/src/main/java/com/example/ubernavidenio/registroViewControler.java
+++ app/src/main/java/com/example/ubernavidenio/registroViewControler.java
@@ -2,7 +2,10 @@ package com.example.ubernavidenio;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class registroViewControler extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class registroViewControler extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro_view_controler);
+
+        Button btnRegCliente = (Button) findViewById(R.id.RegistrarUsuario);
+        btnRegCliente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent (v.getContext(), registrar_cliente_view_controler.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
     }
+
+
 }
