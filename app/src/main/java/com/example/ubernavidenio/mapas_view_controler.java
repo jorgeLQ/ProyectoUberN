71f94e193a87e3071685c86ce00be526dbf14f69
alex <alexiscdmovil@live.com>
2020-01-26 18:30:16 -0500

    Registro cuenta mejorado

--- app/src/main/java/com/example/ubernavidenio/mapas_view_controler.java
+++ app/src/main/java/com/example/ubernavidenio/mapas_view_controler.java
@@ -1,68 +1,200 @@
 package com.example.ubernavidenio;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+
 import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.Dialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResult;
+import com.google.android.gms.location.LocationSettingsStates;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
+import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class mapas_view_controler extends FragmentActivity implements OnMapReadyCallback {
+import java.util.Objects;
 
-    private GoogleMap mMap;
+public class mapas_view_controler extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks {
+
+    private static GoogleApiClient mGoogleApiClient;
+    private static final int ACCESS_FINE_LOCATION_INTENT_ID = 3;
+    private static final String BROADCAST_ACTION = "android.location.PROVIDERS_CHANGED";
+
+    private SupportMapFragment mapFragment;
+    private GoogleMap mGoogleMap;
+    private LocationRequest mLocationRequest;
+    private boolean stateMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mapas_view_controler);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        checkPermissions();     //Check Permission
 
-     //  int status =GooglePlayServicesUtil.isGooglePlayServicesAvailable(getApplicationContext());
-       //if(status== ConnectionResult.SUCCESS){
-        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager()
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-    //}
-   // else{
-       // Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status,(Activity)getApplicationContext(),10);
-       // dialog.show();
-    //}
+        stateMap = false;
+        //Este handler  sirve para dar la ubicacion
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                if (!stateMap) {
+                    checkPermissions();     //Check Permission
+                }
+            }
+        }, 10000); //timer
 
     }
+
+    private void checkPermissions() {
+        if (Build.VERSION.SDK_INT >= 23) {
+            if (ContextCompat.checkSelfPermission(mapas_view_controler.this,
+                    android.Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED)
+                requestLocationPermission();
+
+        }
+    }
+
+    private void requestLocationPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(mapas_view_controler.this, android.Manifest.permission.ACCESS_FINE_LOCATION)) {
+            ActivityCompat.requestPermissions(mapas_view_controler.this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_INTENT_ID);
+        } else {
+            ActivityCompat.requestPermissions(mapas_view_controler.this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, ACCESS_FINE_LOCATION_INTENT_ID);
+        }
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        registerReceiver(gpsLocationReceiver, new IntentFilter(BROADCAST_ACTION));//Registrar el receptor de difusión para comprobar el estado del GPS.
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (gpsLocationReceiver != null)
+            unregisterReceiver(gpsLocationReceiver);
+    }
+
+    /* Receptor de difusión para comprobar el estado del GPS */
+    private BroadcastReceiver gpsLocationReceiver = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            //Si la acción es la ubicación
+            if (intent.getAction().matches(BROADCAST_ACTION)) {
+                LocationManager locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+                //Compruebe si el GPS está encendido o apagado
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+                    Log.e("About GPS", "GPS is Enabled in your device");
+                }
+
+
+            }
+        }
+    };
+
+    /* Método de permiso On Request para verificar si el permiso se ha otorgado o no a Marshmallow Devices */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        Log.e("TAG", "onRequestPermissionsResult");
+        switch (requestCode) {
+            case ACCESS_FINE_LOCATION_INTENT_ID: {
+                // Si se cancela la solicitud, las matrices de resultados están vacías.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(mapas_view_controler.this, "Location Permission denied.", Toast.LENGTH_SHORT).show();
+                }
+                return;
+            }
+        }
+    }
+    
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        Log.e("TAG", "onConnected");
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(9000);
+        mLocationRequest.setFastestInterval(9000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        try {
+            if (ContextCompat.checkSelfPermission(Objects.requireNonNull(this), Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, (LocationListener) this);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.e("TAG", "onConnectionSuspended");
+        mGoogleApiClient.connect(i);
+    }
+
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.e("TAG", "onConnectionSuspended");
+    }
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        mMap=googleMap;
-        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-        mMap.getUiSettings().setMyLocationButtonEnabled(true);
-       if(ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED &&
-               ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
-           return;
-       }
-        mMap.setMyLocationEnabled(true);
-
-
-        //UiSettings uiSettings =mMap.getUiSettings();
-        //uiSettings.setZoomControlsEnabled(true);
-
-
-        // Add a marker in Sydney and move the camera
-       //LatLng sydney=new LatLng(-0.288209, 78.403413);
-       //mMap.addMarker(new MarkerOptions().position(sydney).title("Quito").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
-       //float zoomlevel=16;
-       //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, zoomlevel));
+        Log.e("TAG", "onMapReady");
+        mGoogleMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        mGoogleMap.setMyLocationEnabled(true);
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+            } else {
+                Toast.makeText(this, "No cuentas con los permisos necesarios, cierra y abre de nuevo la aplicación", Toast.LENGTH_SHORT).show();
+            }
+        } else {
+        }
     }
+
+
 }
