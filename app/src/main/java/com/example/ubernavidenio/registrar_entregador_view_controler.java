2aa6211d79d34bd71782103520c38eac3e16ae49
alex <alexiscdmovil@live.com>
2020-01-29 19:00:37 -0500

    conexion a la base de datos

--- app/src/main/java/com/example/ubernavidenio/registrar_entregador_view_controler.java
+++ app/src/main/java/com/example/ubernavidenio/registrar_entregador_view_controler.java
@@ -1,7 +1,5 @@
 package com.example.ubernavidenio;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
@@ -9,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
