933d63be58a2ea0478715c8e311e68b9a07584bf
JORGE LUIS QUILUMBA TOAQUIZA <jorge.quilumba@epn.edu.ec>
2020-02-02 09:51:38 -0500

    renombrar Variables

--- app/src/main/java/com/example/ubernavidenio/registrar_cliente_view_controler.java
+++ app/src/main/java/com/example/ubernavidenio/registrar_cliente_view_controler.java
@@ -1,17 +1,17 @@
 package com.example.ubernavidenio;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-
+import android.text.TextUtils;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-
-
-//import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class registrar_cliente_view_controler extends AppCompatActivity {
 
@@ -32,7 +32,7 @@ public class registrar_cliente_view_controler extends AppCompatActivity {
     private Button CANCELARC;
 
 
-   // private FirebaseAuth mAuth;
+    private FirebaseAuth mAuth;
 
     //Variable de datos que van a ser registrados
 
@@ -55,18 +55,88 @@ public class registrar_cliente_view_controler extends AppCompatActivity {
         setContentView(R.layout.activity_registrar_cliente_view_controler);
 
 
+        mAuth = FirebaseAuth.getInstance();
+
+
+        TxtUserNamecl = (EditText) findViewById(R.id.TxtUserNamecl);
+        TxtContrasenacl = (EditText) findViewById(R.id.TxtContrasenacl);
+        TxtCContrasenacl = (EditText) findViewById(R.id.TxtCContrasenacl);
+        TxtNombrecl = (EditText) findViewById(R.id.TxtNombrecl);
+        TxtApellidocl = (EditText) findViewById(R.id.TxtApellidocl);
+        TxtEmailcl = (EditText) findViewById(R.id.TxtEmailcl);
+        TxtNCBancariacl = (EditText) findViewById(R.id.TxtNCBancariacl);
+        Masculinocl = (RadioButton) findViewById(R.id.Masculino);
+        Femeninocl = (RadioButton) findViewById(R.id.Femenino);
+        TxtFNacimientocl = (EditText) findViewById(R.id.TxtFNacimientocl);
+        TxtDireccioncl = (EditText) findViewById(R.id.TxtDireccioncl);
+
+
+        REGISTRARC = (Button) findViewById(R.id.REGISTRARC);
+        CANCELARC = (Button) findViewById(R.id.CANCELARC);
 
 
 
 
 
+        REGISTRARC.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userCli = TxtUserNamecl.getText().toString();
+                contrase = TxtContrasenacl.getText().toString();
+                ccontrase = TxtCContrasenacl.getText().toString();
+                nameCli = TxtNombrecl.getText().toString();
+                apecli = TxtApellidocl.getText().toString();
+                mail = TxtEmailcl.getText().toString();
+                cuenta = TxtNCBancariacl.getText().toString();
+                fechana = TxtFNacimientocl.getText().toString();
+                direccion = TxtDireccioncl.getText().toString();
+
+            }
+
+        });
 
 
+
+        /*
+        Button btnRegTarjet = (Button) findViewById(R.id.REGISTRARC);
+        btnRegTarjet.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), registrar_targeta_view_controler.class);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        */
+
+
+
+
+        /*
+        Button cerrar= (Button) findViewById(R.id.CANCELARC);
+        cerrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        */
 
 
 
     }
     private void registrarUsuarioCl(){
+        String UNamecl=TxtUserNamecl.getText().toString().trim();
+        String Contrasenacl=TxtContrasenacl.getText().toString().trim();
+        String CContrasenacl=TxtCContrasenacl.getText().toString().trim();
+        String Nombrecl=TxtNombrecl.getText().toString().trim();
+        String Apellidocl=TxtApellidocl.getText().toString().trim();
+        String Emailcl=TxtEmailcl.getText().toString().trim();
+        String CBancariacl=TxtNCBancariacl.getText().toString().trim();
+        String FNacimientocl=TxtFNacimientocl.getText().toString().trim();
+        String Direccioncl=TxtDireccioncl.getText().toString().trim();
+
 
 
 
